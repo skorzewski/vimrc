718b59f95d95a568499b91722ce799187a684b01
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2020-08-06 12:13:38 +0200

    Tmux setup

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ ln -s ${CURRENT_DIR}/vimrc ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ${VIM_PLUGINS_DIR}/Vundle.vim
 
-sudo apt-get install astyle cppcheck pep8 pylint3 || echo 'Not a Debian-based distro. Install desired tools on your own.'
+# sudo apt-get install astyle cppcheck pep8 pylint3 || echo 'Not a Debian-based distro. Install desired tools on your own.'
 
 vim +PluginInstall +qall
 
@@ -20,3 +20,15 @@ mkdir -p ~/.vim/colors
 cp solarized.vim ~/.vim/colors/
 
 cat gtk.css >>.config/gtk-3.0/gtk.css
+
+
+# Set up Tmux
+
+sudo apt install tmux
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+[ -f ~/.tmux.conf ] && rm ~/.tmux.conf
+ln -s ${CURRENT_DIR}/tmux.conf ~/.tmux.conf
+
+tmux source-file ~/.tmux.conf
